aeb4428aea8fe3caa55415d2c68e05b693d62683
Kaitlyn <kaitlyn.a.goodman@gmail.com>
2021-07-28 03:52:37 +0000

    defined word count function completed

--- wordcount.py
+++ wordcount.py
@@ -1,1 +1,16 @@
 # put your code here.
+def word_count(file):
+    filename = open(file)
+
+    word_count = {}
+
+    for line in filename:
+        words = line.rstrip().split(" ")
+
+        for word in words:
+            word = word.rstrip(",).?!'\"_")
+            word = word.lstrip("(\"'_")
+            word_count[word] = word_count.get(word, 0) + 1
+    return word_count
+
+print(word_count("twain.txt"))
